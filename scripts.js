f1d1a3cead0fead5632930554f6377e101202645
joshua-hurn <joshua.hurn@hotmail.com>
2018-09-28 16:58:16 -0500

    end of day

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
-let genBtn = document.getElementById('generateBtn');
-
-class Die {
+class Dice {
     constructor(value) {
         this.value = value;
-        this.div = document.createElement('div')
+        this.div = document.createElement('div');
+        this.div.classList.add('dice');
+        document.getElementById('dice-container').appendChild(this.div);
+        document.getElementById('generateBtn').addEventListener('click', () => {
+            document.createElement('div');
+            let numGen = Math.floor((Math.random() * 6) + 1);
+            this.div.innerHTML = numGen;
+        })
     }
     roll() {
         let numGen = Math.floor((Math.random() * 6) + 1);
@@ -11,8 +16,8 @@ class Die {
     }
 }
 
-genBtn.addEventListener('click', () => {
-    document.createElement('div');
-    roll();
-    document.getElementById('dice-container').appendChild(this.div);
+document.getElementById('generateBtn').addEventListener('click', () => {
+    new Dice();
+    let numGen = Math.floor((Math.random() * 6) + 1);
+    this.div.innerHTML = numGen;
 })
